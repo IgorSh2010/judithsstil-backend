598d9fc892b9aed9c89ecd91d48b5b2fe017f1d2
IgorSh2010 <Igorsh2010@gmail.com>
2025-10-15 12:07:28 +0200

    Updated from VS Studio code

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -26,11 +26,13 @@ export const authenticateToken = (req, res, next) => {
 
     // 3) верифікуємо
     const decoded = jwt.verify(token, JWT_SECRET);
+    if (!decoded) return res.status(401).json({ message: "Недійсний токен або невалідний" });
+    
     // підкладемо payload у req.user
     req.user = decoded;
     next();
   } catch (err) {
     console.error("authMiddleware error:", err);
-    return res.status(401).json({ message: "Невалідний або прострочений токен" });
+    return res.status(401).json({ message: "Якісь інші проблеми з токеном" });
   }
 };
