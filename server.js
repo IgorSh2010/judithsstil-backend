056411a497937fdc221efcc64b21f3042ceabece
IgorSh2010 <Igorsh2010@gmail.com>
2025-10-15 15:23:12 +0200

    Updated from VS Studio code

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import { pool } from "./middleware/dbConn.js";
 import { authenticateToken } from "./middleware/authMiddleware.js"; 
 import authRoutes from "./routes/auth.js"; 
-//import userRoutes from "./routes/users.js";
+import userRoutes from "./routes/users.js";
 import dotenv from "dotenv";
 
 dotenv.config();
