1315dcd2339db4a54bb9e4fd71f1f04af4c152e1
IgorSh2010 <Igorsh2010@gmail.com>
2025-10-16 16:43:17 +0200

    Updated from VS Studio code

--- routes/products.js
+++ routes/products.js
@@ -1,11 +1,11 @@
 import express from "express";
 import multer from "multer";
 import { createProduct } from "../controllers/productController.js";
-import { authenticateToken } from "../middleware/authMiddleware.js";
+//import { authenticateToken } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 const upload = multer({ dest: "tmp/" }); // тимчасова папка
 
-router.post("/create", authenticateToken, upload.array("images", 8), createProduct);
+router.post("/create", upload.array("images", 8), createProduct); //authenticateToken
 
 export default router;
