d3ac21e41dbc8f50871fca34957318a69f6de3ff
IgorSh2010 <Igorsh2010@gmail.com>
2025-10-18 23:44:15 +0200

    Updated from VS Studio code

--- controllers/productController.js
+++ controllers/productController.js
@@ -16,6 +16,7 @@ export const createProduct = async (req, res) => {
   const client = req.dbClient;
   const { name, description, price } = req.body;
   const files = req.files;
+  const uploaded = [];
 
   if (!name || !price) {
     return res.status(400).json({ message: "Brak wymaganych danych." });
@@ -32,7 +33,6 @@ export const createProduct = async (req, res) => {
     const productId = result.rows[0].id;
 
     // 2️⃣ Завантажуємо фото на Cloudinary
-    const uploadedUrls = [];
     if (files && files.length > 0) {
       for (const file of files) {
         const shortName = uuidv4().slice(0, 18); // типу "f2a4c1e8b9"
@@ -41,7 +41,8 @@ export const createProduct = async (req, res) => {
           public_id: shortName, // Cloudinary сам додасть розширення
           resource_type: "image",
         });
-        uploadedUrls.push({
+
+        uploaded.push({
                             url: uploadResult.secure_url,
                             public_id: uploadResult.public_id,
                           });
@@ -50,7 +51,7 @@ export const createProduct = async (req, res) => {
     }
 
     // 3️⃣ Зберігаємо URL у базі
-    if (uploadedUrls.length > 0) {
+    if (uploaded.length > 0) {
       const insertImageQuery = `
         INSERT INTO product_images (product_id, image_url, public_id)
         VALUES ${uploaded.map((_, i) => `($1, $${i * 2 + 2}, $${i * 2 + 3})`).join(", ")} 
@@ -64,9 +65,9 @@ export const createProduct = async (req, res) => {
       await client.query(insertImageQuery, params);
     }
 
-    res.status(201).json({
+    res.json({
       message: "Produkt pomyślnie dodany!",
-      product: { id: productId, name, price, images: uploadedUrls },
+      //product: { id: productId, name, price, images: uploadedUrls },
     });
   } catch (err) {
     console.error("❌ Błąd dodawania produktu:", err);
