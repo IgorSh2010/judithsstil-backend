7b388ed7bd4b2edb26487c47d06ae1d0166365b5
IgorSh2010 <Igorsh2010@gmail.com>
2025-10-22 12:35:00 +0200

    Updated from VS Studio code

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-              origin: "http://localhost:3000", // або ['http://localhost:3000', 'https://твійдомен.pl']
+              origin: ['http://localhost:3000', 'https://judithsstil.vercel.app/'],
               methods: ["GET", "POST", "PUT", "DELETE"],
               credentials: true,
             }));
