828e94435bfb8a62983883598d92e053bd81acd8
IgorSh2010 <Igorsh2010@gmail.com>
2025-10-24 13:24:39 +0200

    Updated from VS Studio code

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,12 @@ import express from "express";
 import { userUpdate } from "../controllers/userController.js";
 import { uploadImage } from "../controllers/settingsController.js";
 import { authenticateToken } from "../middleware/authMiddleware.js";
+import { tenantResolver } from "../middleware/tenantResolver.js";
 
 const router = express.Router();
 
 router.post("/update", authenticateToken, userUpdate); //, 
-router.post("/upload-image", authenticateToken, uploadImage);  
+router.post("/upload-image", tenantResolver, authenticateToken, uploadImage);  
 
 // router.get("/admin-only", authMiddleware, requireRole("admin"), (req, res) => {
 //   res.json({ ok: true });
