566fb4ec3dd8b92e9cfc6246575b5e20609b60cd
IgorSh2010 <Igorsh2010@gmail.com>
2025-10-25 10:07:01 +0200

    Updated from VS Studio code

--- controllers/settingsController.js
+++ controllers/settingsController.js
@@ -16,11 +16,13 @@ export const uploadImage = async (req, res) => {
   const file = req.file;
   const type = req.type;
   const userId = req.user.id;
+  console.log("Uploading image for user ID:", userId, "Type:", type);
     if (!file) {
     return res.status(400).json({ message: "Brak pliku do wgrania." });
   }
     try {
     // Збереження URL у базі
+    console.log("Preparing to save image URL to database.");
     const insertImageQuery = `
       INSERT INTO settings (user_id, ${type}_url, public_id, updated_at)
       VALUES ($1, $2, $3, NOW())
@@ -32,6 +34,9 @@ export const uploadImage = async (req, res) => {
       RETURNING ${type}_url AS image_url;
     `;
 
+    console.log("Insert Image Query:", insertImageQuery);
+    console.log("Uploading file to Cloudinary:", file.path);
+
     // Завантаження фото на Cloudinary
     const shortName = uuidv4().slice(0, 18);
     const uploadResult = await cloudinary.uploader.upload(file.path, {  
@@ -39,13 +44,17 @@ export const uploadImage = async (req, res) => {
         public_id: shortName, // Cloudinary сам додасть розширення
         resource_type: "image",
     });
+
+      console.log("Upload Result:", uploadResult);
+
     fs.unlinkSync(file.path); // видалення тимчасового файлу
-    
+
     const result = await client.query(insertImageQuery, [
       userId,
       uploadResult.secure_url,
       uploadResult.public_id,
     ]);
+    console.log("Database insert result:", result.rows[0]);
     res.json({
       message: "Obraz pomyślnie wgrany!",
       imageUrl: result.rows[0].image_url,
--- routes/users.js
+++ routes/users.js
@@ -8,8 +8,8 @@ import { tenantResolver } from "../middleware/tenantResolver.js";
 const router = express.Router();
 const upload = multer({ dest: "tmp/" }); // тимчасова папка
 
-router.post("/update", authenticateToken, userUpdate); //, 
-router.post("/upload-image", tenantResolver, authenticateToken, upload.single("image"), uploadImage);  
+router.post("/update", authenticateToken, userUpdate); //,authenticateToken 
+router.post("/upload-image", tenantResolver, upload.single("image"), uploadImage);  
 
 // router.get("/admin-only", authMiddleware, requireRole("admin"), (req, res) => {
 //   res.json({ ok: true });
