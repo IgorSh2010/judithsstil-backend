8feefb036361bc4c3e32230101ade4c0e9f48d99
IgorSh2010 <Igorsh2010@gmail.com>
2025-10-28 19:24:28 +0100

    Updated from VS Studio code

--- controllers/publicController.js
+++ controllers/publicController.js
@@ -60,3 +60,26 @@ export const getCategories = async (req, res) => {
         res.status(500).json({ message: "Błąd serwera podczas pobierania kategorji." });
     }
 };
+
+export const getProducts = async (req, res) => {  
+  try {
+    const query = `
+      SELECT p.id, p.title as name, p.description, p.price, pc.name as category, p.is_available,
+        COALESCE(json_agg(pi.image_url) FILTER (WHERE pi.image_url IS NOT NULL), '[]') AS images
+      FROM judithsstil.products p
+      LEFT JOIN judithsstil.product_images pi ON p.id = pi.product_id
+      LEFT JOIN judithsstil.product_categories pc ON p.category_id = pc.id
+      GROUP BY p.id
+      ORDER BY p.created_at DESC;
+    `;
+    const result = await client.query(query);
+    const products = result.rows.map((p) => ({
+      ...p,
+      images: typeof p.images === "string" ? JSON.parse(p.images) : p.images
+    }));
+    res.json(products);
+  } catch (err) {
+    console.error("❌ Błąd pobierania produktów:", err);
+    res.status(500).json({ message: "Błąd serwera" });
+  }
+};
--- routes/public.js
+++ routes/public.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { getLogo, getBanner, getCategories } from "../controllers/publicController.js";
+import { getLogo, getBanner, getCategories,
+         getProducts } from "../controllers/publicController.js";
 
 const router = express.Router();
 
 router.get("/logo", getLogo);
 router.get("/banner", getBanner);
-router.get("/categories", getBanner);
+router.get("/categories", getCategories);
+router.get("/getProducts", getProducts);
 
 export default router;
