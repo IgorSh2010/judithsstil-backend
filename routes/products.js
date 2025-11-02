c46f494cf8c5dc47461773e0bcd5dcd21f559d07
IgorSh2010 <Igorsh2010@gmail.com>
2025-11-02 20:30:57 +0100

    Updated from VS Studio code

--- routes/products.js
+++ routes/products.js
@@ -1,6 +1,6 @@
 import express from "express";
 import multer from "multer";
-import { createProduct, updateProduct, getProducts, changeAvailability, deleteProduct } from "../controllers/productController.js";
+import { createProduct, updateProduct, getProducts, deleteProduct } from "../controllers/productController.js";
 import { tenantResolver } from "../middleware/tenantResolver.js";
 import { authenticateToken } from "../middleware/authMiddleware.js";
 
@@ -9,7 +9,7 @@ const upload = multer({ dest: "tmp/" }); // тимчасова папка
 
 router.post("/create", tenantResolver, authenticateToken, upload.array("images", 8), createProduct);
 router.get("/get", tenantResolver, getProducts); //authenticateToken
-router.put("/:id/availability", tenantResolver, changeAvailability); 
+//router.put("/:id/availability", tenantResolver, changeAvailability); 
 router.put("/update/:id", tenantResolver, authenticateToken, upload.array("images", 7), updateProduct);
 //router.put("upload-image/:id", tenantResolver, authenticateToken,  uploadImages);
 router.delete("/:id", tenantResolver, authenticateToken, deleteProduct); //authenticateToken 
