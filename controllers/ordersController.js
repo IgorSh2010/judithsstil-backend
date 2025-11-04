030911fbfc7de0581c0b8151358be98784d8fd8c
IgorSh2010 <Igorsh2010@gmail.com>
2025-11-04 18:37:51 +0100

    Updated from VS Studio code

--- controllers/ordersController.js
+++ controllers/ordersController.js
@@ -3,11 +3,35 @@ export const getClientOrder = async (req, res) => {
     const client = req.dbClient;
     try {
         if (id !== "main") {
-            const result = await client.query(`SELECT o.*, os.label as status_label  FROM orders o
-                                                left join order_statuses os 
-                                                    on o.status_id = os.id 
-                                                WHERE o.id = $1`, [id]);
-            res.json(result.rows[0]);
+            const orderQuery  = `SELECT o.*, os.label as status_label  FROM orders o
+                                left join order_statuses os 
+                                    on o.status_id = os.id 
+                                WHERE o.id = $1`;
+
+            const orderResult = await client.query(orderQuery, [id]); 
+
+            const itemsQuery = `
+                                SELECT 
+                                oi.id,
+                                p.id AS product_id,
+                                p.name,
+                                p.price AS product_price,
+                                oi.quantity,
+                                oi.price AS item_price,
+                                (oi.quantity * oi.price) AS total_item
+                                FROM judithsstil.order_items oi
+                                JOIN judithsstil.products p ON p.id = oi.product_id
+                                WHERE oi.order_id = $1
+                            `;
+            const itemsResult = await client.query(itemsQuery, [id]);
+
+            res.json({
+                ...order,
+                items: itemsResult.rows,
+                history: null, //historyResult.rows,
+                payment: null, //paymentResult.rows[0] || null,
+            });
+            
         } else {
             const result = await client.query(`SELECT o.*, os.label as status_label  FROM orders o
                                                 left join order_statuses os 
--- routes/users.js
+++ routes/users.js
@@ -12,7 +12,7 @@ const upload = multer({ dest: "tmp/" }); // тимчасова папка
 router.post("/update", authenticateToken, userUpdate); // 
 router.post("/upload-image", tenantResolver, authenticateToken, upload.single("image"), uploadImage);  
 router.get("/get-image", tenantResolver, authenticateToken, getImage);
-router.get("/client-order", tenantResolver, authenticateToken, getClientOrder);
+//router.get("/client-order", tenantResolver, authenticateToken, getClientOrder);
 router.get("/client-order/:id", tenantResolver, authenticateToken, getClientOrder);
 
 
