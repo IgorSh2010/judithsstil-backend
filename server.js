0e07d336ebc4db9c32dcc575e64f272182b5f669
IgorSh2010 <Igorsh2010@gmail.com>
2025-11-05 19:33:52 +0100

    Updated from VS Studio code

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(cookieParser());
 app.use(express.json());
 
 // 🔹 CORS 
-/* app.use((req, res, next) => {
+app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", req.headers.origin);
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
@@ -38,7 +38,7 @@ app.use(express.json());
     return res.sendStatus(200);
   }
   next();
-}); */
+});
 
 // Маршрути
 app.use("/api/auth", authRoutes); 
