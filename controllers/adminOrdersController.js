fa031695606b9fc4b46e052ceb295c71ed9600ce
IgorSh2010 <Igorsh2010@gmail.com>
2025-11-11 23:47:06 +0100

    Updated from VS Studio code

--- controllers/adminOrdersController.js
+++ controllers/adminOrdersController.js
@@ -1,19 +1,14 @@
 export const getOrders = async (req, res) => {
     const { id } = req.params;
     const client = req.dbClient;
-    const result = {};
 
     try {
-        if (id !== "main") {
-            result = await client.query(
-            "SELECT id, user_id, total_price, status_id, payment_id, description FROM orders WHERE id = $1", [id]
-        );
-        } else {
-            result = await client.query(
-            "SELECT id, user_id, total_price, status_id, payment_id, description FROM orders"
-        );
-        }
+        const query =
+        id !== "main"
+            ? { text: "SELECT id, user_id, total_price, status_id, payment_id, description FROM orders WHERE id = $1", values: [id] }
+            : { text: "SELECT id, user_id, total_price, status_id, payment_id, description FROM orders" };
 
+        const result = await client.query(query);
         res.json(result.rows);
     } catch (err) {
         console.error("❌ Помилка при отриманні замовлень:", err);
