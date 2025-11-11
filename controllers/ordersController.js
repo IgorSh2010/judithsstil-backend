c8e4cc204eae20538cf5605de33bc48db57a56fd
IgorSh2010 <Igorsh2010@gmail.com>
2025-11-11 19:11:48 +0100

    Updated from VS Studio code

--- controllers/ordersController.js
+++ controllers/ordersController.js
@@ -147,10 +147,15 @@ export const addToCart = async (req, res) => {
 export const clearCart = async (req, res) => {
   const client = req.dbClient;
   const user_id = req.user?.id;
-  
+
   try {
     await client.query("BEGIN");
-    await client.query(`DELETE FROM cart_items WHERE cart_id = (SELECT id FROM carts WHERE user_id = $1 AND is_finished = false)`);
+    await client.query(`DELETE FROM cart_items 
+                        WHERE cart_id = (SELECT id FROM carts
+                                         WHERE user_id = $1 
+                                         AND is_finished = false)`
+                        , [user_id]);
+                        
     await client.query("COMMIT");
     res.json({ message: "Koszyk został wyczyszczony." });
   } catch (error) {
