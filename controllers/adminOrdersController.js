0d9ba9ba89899927c3b2e74424928f83ca13cbd0
IgorSh2010 <Igorsh2010@gmail.com>
2025-11-12 12:31:27 +0100

    Updated from VS Studio code

--- controllers/adminOrdersController.js
+++ controllers/adminOrdersController.js
@@ -1,24 +1,51 @@
 export const getOrders = async (req, res) => {
-    const { id } = req.params;
-    const client = req.dbClient;
+  const { id } = req.params;
+  const client = req.dbClient;
 
-    try {
-        const query =
-        id !== "main"
-            ? { text: `SELECT o.id, u.usermame, u.email, u.phone, u.adress, total_price, o.status_id, payment_id, description 
-                        FROM orders o
-                        LEFT JOIN public.users u ON orders.user_id = u.id 
-                        WHERE id = $1`, values: [id] }
-            : { text: `SELECT o.id, u.usermame, u.email, u.phone, u.adress, total_price, o.status_id, payment_id, description 
-                        FROM orders o
-                        LEFT JOIN public.users u ON orders.user_id = u.id` };
+  try {
+    const query =
+      id !== "main"
+        ? {
+            text: `
+              SELECT 
+                o.id, 
+                u.username, 
+                u.email, 
+                u.phone, 
+                u.address, 
+                o.total_price, 
+                o.status_id, 
+                o.payment_id, 
+                o.description
+              FROM orders o
+              LEFT JOIN public.users u ON o.user_id = u.id
+              WHERE o.id = $1
+            `,
+            values: [id],
+          }
+        : {
+            text: `
+              SELECT 
+                o.id, 
+                u.username, 
+                u.email, 
+                u.phone, 
+                u.address, 
+                o.total_price, 
+                o.status_id, 
+                o.payment_id, 
+                o.description
+              FROM orders o
+              LEFT JOIN public.users u ON o.user_id = u.id
+            `,
+          };
 
-        const result = await client.query(query);
-        res.json(result.rows);
-    } catch (err) {
-        console.error("❌ Помилка при отриманні замовлень:", err);
-        res.status(500).json({ message: "Помилка сервера", error: err.message });
-    } finally {
-        client.release();
-    }
+    const result = await client.query(query);
+    res.json(result.rows);
+  } catch (err) {
+    console.error("❌ Помилка при отриманні замовлень:", err);
+    res.status(500).json({ message: "Помилка сервера", error: err.message });
+  } finally {
+    client.release();
+  }
 };
