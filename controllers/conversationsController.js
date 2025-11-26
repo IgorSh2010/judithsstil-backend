1008c0008c330344b2c226f83f488514a70e122c
IgorSh2010 <Igorsh2010@gmail.com>
2025-11-26 12:47:11 +0100

    Updated from VS Studio code

--- controllers/conversationsController.js
+++ controllers/conversationsController.js
@@ -69,6 +69,7 @@ export const fetchMessages = async (req, res) => {
 export const sendMessageToConversation = async (req, res) => {
     const conversationId = req.params.id;
     const { content } = req.body;
+    console.log("body - ", req.body, "content - ", content);
     const client = req.dbClient;
     const userId = req.user?.id;
 
